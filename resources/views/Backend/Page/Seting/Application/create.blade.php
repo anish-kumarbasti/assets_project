84233dc8a9956d34072195515d9a590dc8759816
Anuj kumar Singh <anujsingh854282@gmail.com>
2023-09-07 18:33:37 +0530

    application setting

--- resources/views/Backend/Page/Seting/Application/create.blade.php
+++ resources/views/Backend/Page/Seting/Application/create.blade.php
@@ -1,36 +1,38 @@
 @extends('Backend.Layouts.panel')
+
 @section('Content-Area')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Application Setting</div>
+                <div class="card-header">
+                    <h4>{{ $businessSetting ? 'Update' : 'Add' }} Application Settings</h4>
+                </div>
                 <div class="card-body">
-                    <form method="POST" action="{{ route('settings.application.storeOrUpdate') }}" enctype="multipart/form-data">
+                    <form method="POST" action="{{ $businessSetting ? route('settings.application.storeOrUpdate', $businessSetting->id) : route('settings.application.store') }}" enctype="multipart/form-data">
                         @csrf
+                        @if ($businessSetting)
+                            @method('PATCH')
+                        @endif
                         <div class="form-group">
                             <label for="name">Company Name</label>
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter company name">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter company name" value="{{ $businessSetting->name ?? '' }}">
                         </div>
-                        <input type="hidden" name="id" value="{{ $businessSetting->id ?? '' }}"/>
                         <div class="form-group">
                             <label for="email">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
+                            <input type="email" class="form-control" id="email" name="email" placeholder="Enter email" value="{{ $businessSetting->email ?? '' }}">
                         </div>
-
                         <div class="form-group">
                             <label for="address">Address</label>
-                            <textarea class="form-control" id="address" name="address" rows="4" placeholder="Enter address"></textarea>
+                            <textarea class="form-control" id="address" name="address" rows="4" placeholder="Enter address">{{ $businessSetting->address ?? '' }}</textarea>
                         </div>
-
                         <div class="form-group">
                             <label for="logo">Logo</label>
                             <input type="file" class="form-control-file" id="logo" name="logo">
                             <small class="form-text text-muted">Upload a company logo (if needed).</small>
                         </div>
-
-                        <button type="submit" class="btn btn-primary">Add</button>
-                        <a href="" class="btn btn-secondary">Cancel</a>
+                        <button type="submit" class="btn btn-primary">{{ $businessSetting ? 'Update' : 'Add' }}</button>
+                        <a href="{{ route('settings.application.storeOrUpdate') }}" class="btn btn-secondary">Cancel</a>
                     </form>
                 </div>
             </div>
