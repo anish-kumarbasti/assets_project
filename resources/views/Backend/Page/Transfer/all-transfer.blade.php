13f1258343c0796c2f4ef38ba7959c8954feccfe
Anuj kumar Singh <anujsingh854282@gmail.com>
2023-09-16 21:00:11 +0530

    All transfer complete

---
+++ resources/views/Backend/Page/Transfer/all-transfer.blade.php
@@ -0,0 +1,51 @@
+@extends('Backend.Layouts.panel')
+
+@section('Content-Area')
+<div class="col-sm-12">
+    <div class="card">
+        <div class="card-header pb-0">
+            <h3>All Transfers</h3>
+        </div>
+        <div class="card">
+            <div class="card-body">
+               <div class="table-responsive theme-scrollbar">
+                  <table class="display" id="basic-1">
+                        <thead>
+                            <tr class="text-center">
+                                <th>SL</th>
+                                <th>Transfer ID</th>
+                                <th>Employee</th>
+                                <th>Handover Employee</th>
+                                <th>Transfer Reason</th>
+                                <th>Products</th>
+                                <th>Description</th>
+                                <th>Created At</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($transfers as $transfer)
+                                <tr>
+                                    <td>{{$loop->iteration}}</td>
+                                    <td>{{ $transfer->id }}</td>
+                                    <td>{{ $transfer->employee->employee_id }}</td>
+                                    <td>{{ $transfer->handoverEmployee->employee_id }}</td>
+                                    <td>{{ $transfer->reason->reason }}</td>
+                                    <td>
+                                        <ul>
+                                            @foreach ($transfer->products as $product)
+                                                <li>{{ $product->product_info }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </td>
+                                    <td>{{ $transfer->description }}</td>
+                                    <td>{{ $transfer->created_at }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
