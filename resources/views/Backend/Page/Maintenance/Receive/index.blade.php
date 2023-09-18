9b025b0189a866a45c7c58a522386c18ed04c846
Anish Kumar <123398048+anish-kumarbasti@users.noreply.github.com>
2023-09-18 19:33:36 +0530

    Updated

--- resources/views/Backend/Page/Maintenance/Receive/index.blade.php
+++ resources/views/Backend/Page/Maintenance/Receive/index.blade.php
@@ -14,6 +14,10 @@
         padding: 5px 10px;
         line-height: 1.5;
     }
+
+    .modal-backdrop.show {
+        display: none;
+    }
 </style>
 @endsection
 
@@ -29,7 +33,7 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <form method="post">
+            <form method="post" action="" id="maintenanceForm">
                 @csrf
                 <div class="modal-body">
                     <div class="mb-2">
@@ -42,19 +46,18 @@
                     </div>
                     <div class="mb-4">
                         <label class="form-label" for="validationCustom01">Asset Price:</label>
-                        <input class="form-control" id="price" name="asset_price" type="text" data-bs-original-title="" title="" value="" readonly>
+                        <input class="form-control" id="asset_price" name="asset_price" type="text" data-bs-original-title="" title="" value="" readonly>
                     </div>
                     <div class="mb-2">
                         <label class="form-label">Supplier</label>
-                        <select class="form-select" id="supplier" name="supplier" aria-label="Default select example">
-                            <option value="">--Select Supplier--</option>
-                        </select>
+                        <input class="form-control" id="supplier" name="supplier_id" type="text" data-bs-original-title="" title="" value="" readonly>
                     </div>
                     <div class="mb-2">
                         <label class="form-label">Status</label>
                         <select class="form-select" id="status" name="status" aria-label="Default select example">
-                            <option value="">--Select Status--</option>
-
+                            @foreach ($status as $status)
+                            <option value="{{$status->id}}"><span class="{{$status->status}}">{{$status->name}}</span></option>
+                            @endforeach
                         </select>
                     </div>
                     <div class="mb-2">
@@ -65,10 +68,11 @@
                         <label class="form-label">End Date</label>
                         <input type="date" class="form-control digits" name="end_date" id="end" value="" readonly>
                     </div>
+                    <input type="hidden" id="statusId" name="statusId" value="">
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <a href="" type="submit" class="btn btn-primary">Print</a>
+                    <button type="button" class="btn btn-primary" id="printButton">print</button>
                 </div>
             </form>
         </div>
@@ -112,7 +116,10 @@
                                 <td>{{ $maintenance->end_date }}</td>
                                 <td>
                                     <div class="btn-group">
-                                        <a href="{{ url('maintenance/edit', $maintenance->id) }}" data-toggle="modal" data-target="#updateModal" class="btn btn-primary custom"><i class="fa fa-print "></i> Receive </a>&nbsp;
+                                        <a href="#" data-toggle="modal" data-target="#updateModal" class="btn btn-primary custom receive-button" data-maintenance-id="{{ $maintenance->id }}" data-product-id="{{ $maintenance->product_id }}" data-asset-number="{{ $maintenance->asset_number }}" data-asset-price="{{ $maintenance->asset_price }}" data-supplier="{{ $maintenance->supplier_id }}" data-status="{{ $maintenance->status }}" data-start-date="{{ $maintenance->start_date }}" data-end-date="{{ $maintenance->end_date }}">
+                                            <i class="fa fa-print"></i> Receive
+                                        </a>
+
                                     </div>
                                 </td>
                             </tr>
@@ -131,26 +138,104 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <script>
     $(document).ready(function() {
-        const urlParams = new URLSearchParams(window.location.search);
-        const isModalRequested = urlParams.get('modal');
+        $('.receive-button').on('click', function(e) {
+            const maintenanceId = $(this).data('maintenance-id');
+            const productId = $(this).data('product-id');
+            const assetNumber = $(this).data('asset-number');
+            const assetPrice = $(this).data('asset-price');
+            const supplierId = $(this).data('supplier');
+            const statusName = $(this).data('status');
+            const startDate = $(this).data('start-date');
+            const endDate = $(this).data('end-date');
 
-        if (isModalRequested) {
-            const maintenanceId = urlParams.get('id');
+            $('#product_id').val(productId);
+            $('#asset_number').val(assetNumber);
+            $('#asset_price').val(assetPrice);
+            $('#start').val(startDate);
+            $('#end').val(endDate);
+            $('#status').val(statusName);
+            $('#updateModal').modal('show');
 
             $.ajax({
-                url: `/maintenance/edit/${maintenanceId}`,
+                url: "/get-suppliers/" + supplierId,
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    _token: '{{ csrf_token() }}',
+                },
+                success: function(data) {
+                    console.log(data);
+                    var supplierSelect = $('#supplier');
+                    supplierSelect.empty();
+                    supplierSelect.append($('<option>', {
+                        value: supplierId,
+                        text: data.name
+                    }));
+
+                    supplierSelect.val(supplierId);
+                },
+                error: function() {
+                    console.error('Failed to fetch supplier data.');
+                }
+            });
+
+            $.ajax({
+                url: "get-statuses",
                 type: 'GET',
                 dataType: 'json',
                 success: function(data) {
-                    $('#start').text(data.start_date);
-                    $('#end').text(data.end_date);
-                    $('#updateModal').modal('show');
+                    console.log(data);
+                    var statusSelect = $('#status');
+                    statusSelect.empty();
+                    statusSelect.append($('<option>', {
+                        value: '',
+                        text: ''
+                    }));
+                    $.each(data.statuses, function(key, value) {
+                        statusSelect.append($('<option>', {
+                            value: value.id,
+                            text: value.status
+                        }));
+                    });
+
+                    // Set the selected value
+                    statusSelect.val(status);
                 },
                 error: function() {
-                    console.error('Failed to fetch data.');
+                    console.error('Failed to fetch status data.');
                 }
             });
-        }
+
+
+            $('#printButton').on('click', function(e) {
+                $.ajax({
+                    url: `/maintainans/update/${productId}`,
+                    type: 'post',
+                    data: {
+                        _token: '{{ csrf_token() }}',
+                        product_id: productId,
+                        asset_number: assetNumber,
+                        asset_price: assetPrice,
+                        supplier_id: supplierId,
+                        status: $('#status').val(),
+                        start_date: startDate,
+                        end_date: endDate,
+                    },
+                    success: function(response) {
+                        $('#updateModal').modal('hide');
+                    },
+                    error: function() {
+                        alert('Failed to update data.');
+                    }
+                });
+                alert('Data updated successfully!');
+                e.preventDefault();
+                const printUrl = `maintenance-print/${maintenanceId}`;
+
+                window.location.href = printUrl;
+
+            });
+        });
     });
 </script>
 
